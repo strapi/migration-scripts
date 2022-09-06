1c64783b34b9cd45ed3bd5b8fa3d8cc4e30c15ea
Conrad <conrad.pinnow@montana.edu>
2022-09-06 12:16:56 -0600

    modified stringify call to only invoke on Postgres databases

--- v3-sql-v4-sql/migrate/helpers/adminHelpers.js
+++ v3-sql-v4-sql/migrate/helpers/adminHelpers.js
@@ -57,7 +57,7 @@ async function migrateAdminPermissions() {
       action: migrateUids(item.action),
       subject: migrateSubject(item.subject),
       properties: migrateProperties(item.properties),
-      conditions: item.conditions,
+      conditions: isPGSQL ? JSON.stringify(item.conditions) : item.conditions,
     }));
     const roleLinks = items.map((item) => ({
       permission_id: item.id,
