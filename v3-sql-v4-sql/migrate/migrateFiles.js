507bc43cfe7b1b162734d848d5e058a5232efe30
Vítor Manfredini <vitormanfredini@gmail.com>
2022-09-19 19:18:00 -0300

    changes only (reverted automatic double quotes)

--- v3-sql-v4-sql/migrate/migrateFiles.js
+++ v3-sql-v4-sql/migrate/migrateFiles.js
@@ -1,17 +1,21 @@
-const { migrate } = require('./helpers/migrate');
-const { omit } = require('lodash');
-const { snakeCase } = require('lodash/fp');
-const { dbV3 } = require('../config/database');
-const { migrateUids } = require('./helpers/migrateValues');
+const { migrate } = require("./helpers/migrate");
+const { omit } = require("lodash");
+const { snakeCase } = require("lodash/fp");
+const { dbV3 } = require("../config/database");
+const { migrateUids } = require("./helpers/migrateValues");
 
-const processedTables = ['upload_file', 'upload_file_morph'];
-const newTables = ['files', 'files_related_morphs'];
+const processedTables = ["upload_file", "upload_file_morph"];
+const newTables = ["files", "files_related_morphs"];
 
 async function migrateTables() {
   // TODO have to migrate values
-  console.log('Migrating files');
+  console.log("Migrating files");
 
-  const modelsDefs = await dbV3('core_store').where('key', 'like', 'model_def_%');
+  const modelsDefs = await dbV3("core_store").where(
+    "key",
+    "like",
+    "model_def_%"
+  );
 
   const componentsMap = modelsDefs
     .map((item) => JSON.parse(item.value))
@@ -38,7 +42,7 @@ async function migrateTables() {
       updated_by_id: item.updated_by,
     };
 
-    return omit(newItem, ['created_by', 'updated_by']);
+    return omit(newItem, ["created_by", "updated_by"]);
   });
 
   await migrate(processedTables[1], newTables[1], (item) => {
@@ -48,7 +52,7 @@ async function migrateTables() {
       related_type: componentsMap[item.related_type] || related_type,
     };
 
-    return omit(newItem, ['upload_file_id', 'id']);
+    return omit(newItem, ["upload_file_id", "id"]);
   });
 }
 
