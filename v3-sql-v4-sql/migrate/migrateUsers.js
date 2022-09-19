a06a5a9b5647e5fe7d0604369007571ca942ddd4
Vítor Manfredini <vitormanfredini@gmail.com>
2022-09-19 13:57:01 -0300

    changed users tables, double quotes to SQL query and pluralize.singular to "api::" uids

--- v3-sql-v4-sql/migrate/migrateUsers.js
+++ v3-sql-v4-sql/migrate/migrateUsers.js
@@ -1,31 +1,28 @@
-const { dbV3, dbV4 } = require("../config/database");
-const { BATCH_SIZE } = require("./helpers/constants");
-const { migrate, resetTableSequence } = require("./helpers/migrate");
-const { migrateItems, migrateItem } = require("./helpers/migrateFields");
-const { migrateUserPermissionAction } = require("./helpers/usersHelpers");
+const { dbV3, dbV4 } = require('../config/database');
+const { BATCH_SIZE } = require('./helpers/constants');
+const { migrate, resetTableSequence } = require('./helpers/migrate');
+const { migrateItems, migrateItem } = require('./helpers/migrateFields');
+const { migrateUserPermissionAction } = require('./helpers/usersHelpers');
 
 const processedTables = [
-  "users-permissions_role",
-  "users-permissions_permission",
-  "users-permissions_user",
+  'users-permissions_role',
+  'users-permissions_permission',
+  'users-permissions_user',
 ];
 
 async function migrateUserPermissions() {
-  const source = "users-permissions_permission";
-  const destination = "up_permissions";
-  const destinationLinks = "up_permissions_role_links";
+  const source = 'users-permissions_permission';
+  const destination = 'up_permissions';
+  const destinationLinks = 'up_permissions_role_links';
 
-  const sourceSelect = dbV3(source).where("enabled", true);
+  const sourceSelect = dbV3(source).where('enabled', true);
   const count =
     (await sourceSelect.clone().count().first()).count ||
-    (await sourceSelect.clone().count().first())["count(*)"];
+    (await sourceSelect.clone().count().first())['count(*)'];
   const countTotal =
-    (await dbV3(source).count().first()).count ||
-    (await dbV3(source).count().first())["count(*)"];
+    (await dbV3(source).count().first()).count || (await dbV3(source).count().first())['count(*)'];
 
-  console.log(
-    `Migrating ${count}/${countTotal} items from ${source} to ${destination}`
-  );
+  console.log(`Migrating ${count}/${countTotal} items from ${source} to ${destination}`);
   await dbV4(destinationLinks).del();
   await dbV4(destination).del();
   for (var page = 0; page * BATCH_SIZE < count; page++) {
@@ -53,13 +50,13 @@ async function migrateUserPermissions() {
 }
 
 async function migrateUsersData() {
-  const source = "users-permissions_user";
-  const destination = "up_users";
-  const destinationLinks = "up_users_role_links";
+  const source = 'users-permissions_user';
+  const destination = 'users-permissions_user';
+  const destinationLinks = 'users_permissions_user_role_links';
 
   const count =
     (await dbV3(source).count().first()).count ||
-    (await dbV3(source).clone().count().first())["count(*)"];
+    (await dbV3(source).clone().count().first())['count(*)'];
   console.log(`Migrating ${count} items from ${source} to ${destination}`);
   await dbV4(destinationLinks).del();
   await dbV4(destination).del();
@@ -68,9 +65,7 @@ async function migrateUsersData() {
     const items = await dbV3(source)
       .limit(BATCH_SIZE)
       .offset(page * BATCH_SIZE);
-    const migratedItems = migrateItems(items, ({ role, ...item }) =>
-      migrateItem(item)
-    );
+    const migratedItems = migrateItems(items, ({ role, ...item }) => migrateItem(item));
     const roleLinks = items.map((item) => ({
       user_id: item.id,
       role_id: item.role,
@@ -82,8 +77,8 @@ async function migrateUsersData() {
 }
 
 async function migrateTables() {
-  console.log("Migrating Users");
-  await migrate("users-permissions_role", "up_roles");
+  console.log('Migrating Users');
+  await migrate('users-permissions_role', 'up_roles');
   await migrateUserPermissions();
   await migrateUsersData();
 }
